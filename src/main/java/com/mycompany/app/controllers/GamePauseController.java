20093fba509d67edad234fc36c3a3f6c7215f28e
moasch <moa.schaffer@gmail.com>
2017-08-19 10:03:14 +0200

    started with some ideas but commented it out. Continue tomorrow

---
+++ src/main/java/com/mycompany/app/controllers/GamePauseController.java
@@ -0,0 +1,20 @@
+package com.mycompany.app.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by sibas on 2017-08-19.
+ */
+public class GamePauseController implements Initializable {
+
+    public GamePauseController(){
+
+    }
+
+    public void initialize(URL location, ResourceBundle resourses){
+
+    }
+}
